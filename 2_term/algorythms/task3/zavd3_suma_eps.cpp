1f59574637e719852787426998a6c237cdeff25f
Andrii <bohatyrovandrey@gmail.com>
2022-04-20 15:16:44 +0300

    task4_vector

--- 2_term/algorythms/task3/zavd3_suma_eps.cpp
+++ 2_term/algorythms/task3/zavd3_suma_eps.cpp
@@ -3,32 +3,24 @@
 
 using namespace std;
 
-double power(double x, int n);
-
 int main()
 {
     int n = 0;
-    double a = 0, x, eps, k = 1;
+    double sum = 0, a = 0, x, eps, k = 1;
 
     cout << "Enter |x| < 1: "; cin >> x;
     cout << "Enter eps > 1: "; cin >> eps;
 
+    double c = x;
     do
     {
-        a += k * (pow(x, 2 * n + 1) / (2 * n + 1));
-        cout << "a = " << a;
-        cout << "\t|arctg(x) - a| = " << fabs(atan(x) - a) << endl;
+        a = k * (c / (2 * n + 1));
+        sum += a;
+        cout << "|a| = " << abs(a) << endl;
         n++;
         k *= -1;
-    } while (fabs(atan(x) - a) >= eps);
+        c *= x * x;
+    } while (abs(a) >= eps);
 
-    cout << "Result: \na = " << a << "\tarctg(x) = " << atan(x) << endl;
-}
-
-double power(double x, int n)
-{
-    double y = 1;
-    for (int i = 0; i < n; i++)
-        y *= x;
-    return y;
+    cout << "Result: \na = " << a << "\tsum = " << sum << "\tarctg(x) = " << atan(x) << endl;
 }
--- 2_term/algorythms/task4/task4_vector.cpp
+++ 2_term/algorythms/task4/task4_vector.cpp
@@ -1,3 +1,4 @@
+// 4.У заданому масиві цілих чисел знайти найдовшу підпослідовність елементів, сума цифр яких рівна заданому числу с. Вивести її на екран, вказавши індекси першого і останнього елементів підпослідовності.
 #include <iostream>
 #include <vector>
 
@@ -7,22 +8,36 @@ int main()
 {
     int c; cout << "Enter c: "; cin >> c;
     int n; cout << "Enter size of vector: "; cin >> n;
-    vector<int> v;
+    int* arr = new int[n];
     cout << "Enter vector: ";
     for (int i = 0; i < n; i++)
-    {
-        int temp;
-        cin >> temp;
-        v.push_back(temp);
-    }
+        cin >> arr[i];
 
+    int posStart = 0, posEnd = 0, sum = 0;
     for (int i = 0; i < n; i++)
     {
-        vector<int> sub;
+        sum = 0;
         for (int j = i; j < n; j++)
         {
-            sub.push_back(v[j]);
-            
+            sum += arr[j];
+            if (sum > c)
+                break;
+            else if (sum == c && j - i > posEnd - posStart)
+            {
+                posStart = i;
+                posEnd = j;
+            }
         }
     }
+
+    cout << "Start position: " << posStart;
+    cout << "\tEnd position: " << posEnd;
+    cout << "\nSubsequence: ";
+    for (int i = posStart; i <= posEnd; i++)
+        cout << arr[i] << ' ';
+
+    cout << endl;
 }
+
+// 7, 15, 7 1 6 1 2 8 5 6 -1 -1 -1 1 2 1 2
+// 5, 10, 3 2 4 1 3 1 1 1 1 1
